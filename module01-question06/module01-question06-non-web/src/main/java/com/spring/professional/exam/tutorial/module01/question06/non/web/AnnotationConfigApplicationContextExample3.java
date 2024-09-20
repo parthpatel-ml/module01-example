927418aff61bd3111465fc000c9507501daed19c
parthpatel-ml <parthpatel.ml@gmail.com>
2024-09-20 11:34:13 +0930

    Commit-02

--- module01-question06/module01-question06-non-web/src/main/java/com/spring/professional/exam/tutorial/module01/question06/non/web/AnnotationConfigApplicationContextExample3.java
+++ module01-question06/module01-question06-non-web/src/main/java/com/spring/professional/exam/tutorial/module01/question06/non/web/AnnotationConfigApplicationContextExample3.java
@@ -1,12 +1,14 @@
 package com.spring.professional.exam.tutorial.module01.question06.non.web;
 
 import com.spring.professional.exam.tutorial.module01.question06.non.web.beans.SpringBean1;
+import com.spring.professional.exam.tutorial.module01.question06.non.web.defined.configuration.ConfigurationStatic;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AnnotationConfigApplicationContextExample3 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-
+        /*context.register(ConfigurationStatic.class);
+        context.registerBean(ConfigurationStatic.class, SpringBean1.class);*/
         context.scan("com.spring.professional.exam.tutorial.module01.question06.non.web.beans");
         context.refresh();
 
